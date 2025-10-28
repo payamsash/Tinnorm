0694012fae72cb6d53c4a778ffcf27990c6a2126
payam <payam.sadeghi74@gmail.com>
2025-10-28 14:24:36 +0100

    feature extraction added

---
+++ src/03_source_localization.py
@@ -0,0 +1,154 @@
+from mne import read_epochs
+
+from pathlib import Path
+import zipfile
+import numpy as np
+import pandas as pd
+from scipy.signal import welch
+from mne_connectivity import spectral_connectivity_time
+
+from mne import (set_log_level,
+                    read_epochs,
+                    read_labels_from_annot,
+                    extract_label_time_course,
+                    make_forward_solution
+                    )
+from mne.minimum_norm import (read_inverse_operator,
+                                apply_inverse_epochs,
+                                make_inverse_operator,
+                                write_inverse_operator)
+
+
+def compute_source_features(preproc_level, bids_root, atlas="aparc"):
+
+    overwrite = True
+    site_map = {
+        "1": "austin",
+        "2": "dublin",
+        "3": "ghent",
+        "4": "illinois",
+        "5": "regensburg",
+        "6": "tuebingen",
+        "7": "zuerich"
+        }
+
+    site = site_map.get(str(subject_id)[0], "unknown")
+    inv_dir = bids_root.parent / "invs"
+    inv_fname = inv_dir / f"{site}-inv.fif"
+
+    ## read epochs
+    epochs_fname = bids_root / f"sub-{subject_id}" / "ses-01" / "eeg" / f"preproc_{preproc_level}-epo.fif"
+    epochs = read_epochs(epochs_fname, preload=True)
+
+    ## read labels
+    if atlas == "aparc":
+        labels = read_labels_from_annot(subject="fsaverage", subjects_dir=None, parc=atlas)[:-1]
+    if atlas == "aparc.a2009s":
+        labels = read_labels_from_annot(subject="fsaverage", subjects_dir=None, parc=atlas)[:-2]
+
+    ## read_inverse_operator
+    if inv_fname.isdir():
+        inverse_operator = read_inverse_operator(inv_fname)
+
+    else:
+        noise_cov = make_ad_hoc_cov(epochs.info)
+        fs_dir = fetch_fsaverage()
+        trans = fs_dir / "bem" / "fsaverage-trans.fif"
+        src = fs_dir / "bem" / "fsaverage-ico-5-src.fif"
+        bem = fs_dir / "bem" / "fsaverage-5120-5120-5120-bem-sol.fif"
+        fwd = make_forward_solution(
+                                    epochs.info,
+                                    trans=trans,
+                                    src=src,
+                                    bem=bem,
+                                    meg=False,
+                                    eeg=True
+                                    )
+        inverse_operator = make_inverse_operator(
+                                                epochs.info,
+                                                fwd,
+                                                noise_cov
+                                                )
+        write_inverse_operator(
+                            fname=inv_fname,
+                            inv=inverse_operator,
+                            overwrite=overwrite
+                            )
+
+    ## extract brain label signals
+    stcs = apply_inverse_epochs(
+                                epochs,
+                                inverse_operator,
+                                lambda2=1.0 / (1.0 ** 2),
+                                method="dSPM",
+                                label=None,
+                                pick_ori="normal",
+                                return_generator=False
+                                )
+    label_ts = extract_label_time_course(
+                                        stcs,
+                                        labels,
+                                        inverse_operator["src"],
+                                        mode="mean_flip",
+                                        return_generator=False,
+                                        )
+    
+    ## compute power in labels
+    label_ts = np.array(label_ts)
+    n_epochs, n_labels, n_times = label_ts.shape
+    reshaped_data = label_ts.reshape(-1, n_times)
+    freqs, psd = welch(reshaped_data, epochs.info["sfreq"], axis=-1, nperseg=min(256, n_times))
+
+    ## write it to df
+    columns = []
+    labels_power = []
+    for key, value in freq_bands.items():
+        min_freq, max_freq = list(value)
+        band_mask = (freqs >= min_freq) & (freqs <= max_freq)
+        band_powers = np.trapz(psd[:, band_mask], freqs[band_mask], axis=-1)
+        labels_power.append(band_powers.reshape(n_epochs, n_channels))
+        columns += [f"{lb.name}_{key}" for lb in labels]
+
+    labels_power = np.concatenate(labels_power, axis=1)
+    df_power = pd.DataFrame(labels_power, columns=columns)
+
+    ## compute connectivity (pli, plv, coh)
+    lb_names = [lb.name for lb in labels]
+    i_lower, j_lower = np.tril_indices_from(np.zeros(shape=(label_ts.shape[1], label_ts.shape[1])), k=-1)
+    columns = []
+    freq_cons = []
+    for key, value in freq_bands.items(): 
+        con = spectral_connectivity_time(
+                                        label_ts,
+                                        freqs=np.arange(value[0], value[1], 5),
+                                        method=connectivity_method,
+                                        average=False,
+                                        sfreq=info["sfreq"],
+                                        mode="cwt_morlet",
+                                        fmin=value[0],
+                                        fmax=value[1],
+                                        faverage=True,
+                                        n_cycles=value[1] / 6
+                                        )
+        con_matrix = np.squeeze(con.get_data(output="dense")) # n_epochs * n_labels * n_labels
+
+        cons = []
+        for ep_con in con_matrix:
+            ep_con_value = ep_con[i_lower, j_lower]
+            cons.append(ep_con_value)
+        cons = np.array(cons)
+        freq_cons.append(cons)
+
+        con_labels = [f"{lb_names[i]}_vs_{lb_names[j]}_{key}" for i, j in zip(i_lower, j_lower)]
+        columns += con_labels
+    
+    freq_cons = np.concatenate(freq_cons, axis=-1)
+    df = pd.DataFrame(freq_cons, columns=columns)
+
+
+
+# compute aperiodic param per whole recording
+
+
+
+## create dfs out ot it (transpose them and zip them) 1. df_power, 2. df_aperiodic 3. df_connectivity
