e2b43efc8d09804d016d31c4781d5c261d47afb3
payam <payam.sadeghi74@gmail.com>
2026-01-05 12:44:54 +0100

    working with deviation maps initiated

---
+++ src/09_map_deviations.py
@@ -0,0 +1,21 @@
+'''
+## create deviation topoplots by number of subjects deviating z-thr per freq band
+## same for source maps
+## same for conn map
+## same for aperiodic
+
+## correlate deviations with clinical outcomes
+
+
+
+## per connection, how many subjects deviated (skip region level and network level)
+## per power
+## aperiodic
+'''
+
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
