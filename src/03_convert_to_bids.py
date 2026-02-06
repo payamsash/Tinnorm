0d1958a25d9c444442313a67849fdaf0f093a100
payam <payam.sadeghi74@gmail.com>
2026-02-06 07:39:07 +0100

    sampling frequency check add

--- src/03_convert_to_bids.py
+++ src/03_convert_to_bids.py
@@ -4,7 +4,7 @@ from mne_bids import BIDSPath, write_raw_bids
 from tqdm.contrib import tzip
 from tqdm import tqdm
 from mne import set_log_level
-set_log_level("Error")
+#set_log_level("Error")
 
 """
 Convert multi-site EEG datasets to BIDS format.
@@ -27,7 +27,7 @@ def convert_to_bids():
     bids_dir = root_dir / "BIDS"
 
 
-    sites = ["ghent"]
+    sites = ["zuerich"]
 
     for site in sites:
         data_dir = root_dir / site
@@ -93,10 +93,12 @@ def convert_to_bids():
                                         total=len(subject_ids),
                                         desc=f"Converting {site}"):
             print(subject_id)
-            if subject_id == "70005": # tlyd
+            if subject_id not in ["70086"]:
                 continue
-            
+
             raw = read_raw(fname)
+            if raw.info["sfreq"] != 1000.0:
+                raw.resample(1000)
             bids_path = BIDSPath(
                 subject=subject_id,
                 session="01",
